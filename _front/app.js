c7803593af57099242c88e0756104016953b732c
Besnard Julian <besnardjulian61@gmail.com>
2020-06-15 13:26:46 +0200

    Optimisation du code

--- _front/app.js
+++ _front/app.js
@@ -4,7 +4,6 @@ const morgan = require ('morgan')('dev')
 const twig = require("twig")
 const axios = require('axios')
 
-
 //Variables globales
 const app = express()
 const port = 3001
@@ -22,8 +21,9 @@ app.get('/', (req, res) => {
     res.redirect("http://localhost:3001/annuaire")
 })
 
+//Liste de tous les membres
 app.get('/annuaire', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire')
+    fetch.get('/')
     .then((response) => {
         if (response.data.status == 'success') {
             res.render('employes.twig', {
@@ -37,100 +37,83 @@ app.get('/annuaire', (req, res) => {
     .catch((err) => renderError(res, err.message))
 })
 
+//Interface de recherche par nom
+app.get('/annuaire/search/nom', (req, res) => {
+    renderSearch(res, 'nom', 'nom', 'Nom')
+})
+
+//Interface de recherche par prénom
+app.get('/annuaire/search/prenom', (req, res) => {
+    renderSearch(res, 'prénom', 'prenom', 'Prénom')
+})
+
+//Interface de recherche par numéro de téléphone
+app.get('/annuaire/search/phone', (req, res) => {
+    renderSearch(res, 'numéro de téléphone', 'phone', '0xxxxxxxxx')
+})
+
+//Interface de recherche par date d'entrée
+app.get('/annuaire/search/date', (req, res) => {
+    renderSearch(res, "date d'entrée", 'date')
+})
+
+//Interface de recherche par service
+app.get('/annuaire/search/service', (req, res) => {
+    renderSearch(res, 'service', 'service', 'Service-du-contact')
+})
+
+//Recherche par nom
 app.get('/annuaire/search/nom=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/search/nom='+req.params.id)
+    fetch.get('/search/nom='+req.params.id)
     .then((response) => {
-        if (response.data.status == 'success' && response.data.result != "") {
-            res.render('result.twig', {
-                title:'Recherche par nom',
-                employes: response.data.result,
-                param: "Recherche : " + req.params.id
-            })
-        }
-        else
-            //renderError(res, response.data.message)
-            res.render('error.twig', {
-                errorMsg:'Aucun résultat',
-            })
+        renderCheck(res, 'nom', 'nom', response, req.params.id)
     })
     .catch((err) => renderError(res, err.message))
 })
 
+//Recherche par prénom
 app.get('/annuaire/search/prenom=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/search/prenom='+req.params.id)
+    fetch.get('/search/prenom='+req.params.id)
     .then((response) => {
-        if (response.data.status == 'success' && response.data.result != "") {
-            res.render('result.twig', {
-                title:'Recherche par prénom',
-                employes: response.data.result,
-                param: "Recherche : " + req.params.id
-            })
-        }
-        else
-            //renderError(res, response.data.message)
-            res.render('error.twig', {
-                errorMsg:'Aucun résultat',
-            })
+        renderCheck(res, 'prénom', 'prenom', response, req.params.id)
     })
     .catch((err) => renderError(res, err.message))
 })
 
+//Recherche par numéro de téléphone
 app.get('/annuaire/search/phone=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/search/phone='+req.params.id)
+    fetch.get('/search/phone='+req.params.id)
     .then((response) => {
-        if (response.data.status == 'success' && response.data.result != "") {
-            res.render('result.twig', {
-                title:'Recherche par téléphone',
-                employes: response.data.result,
-                param: "Recherche : " + req.params.id
-            })
-        }
-        else
-            //renderError(res, response.data.message)
-            res.render('error.twig', {
-                errorMsg:'Aucun résultat',
-            })
+        renderCheck(res, 'numéro de téléphone', 'phone', response,  req.params.id)
     })
     .catch((err) => renderError(res, err.message))
 })
 
+//Recherche par service
 app.get('/annuaire/search/service=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/search/service='+req.params.id)
+    fetch.get('/search/service='+req.params.id)
     .then((response) => {
-        if (response.data.status == 'success') {
-            res.render('result.twig', {
-                title:'Recherche par service',
-                employes: response.data.result,
-                param: "Recherche : " + req.params.id
-            })
-        }
-        else
-            renderError(res, response.data.message)
+        renderCheck(res, 'service', 'service', response,  req.params.id)
     })
     .catch((err) => renderError(res, err.message))
 })
 
+//Recherche par date d'entrée
 app.get('/annuaire/search/date=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/search/date='+req.params.id)
+    fetch.get('/search/date='+req.params.id)
     .then((response) => {
-        if (response.data.status == 'success') {
-            res.render('result.twig', {
-                title:"Recherche par date d'entrée",
-                employes: response.data.result,
-                param: "Recherche : " + req.params.id
-            })
-        }
-        else
-            renderError(res, response.data.message)
+        renderCheck(res, "date d'entrée", 'date', response, req.params.id)
     })
     .catch((err) => renderError(res, err.message))
 })
 
+
+//Ajout d'un membre
 app.get('/annuaire/add=:id', (req, res) => {
-    fetch.get('http://localhost:3000/annuaire/add='+req.params.id)
+    fetch.get('/add='+req.params.id)
     .then((response) => {
         if (response.data.status == 'success') {
-            res.render('result.twig', {
+            res.render('added.twig', {
                 title:req.params.id + " employés ajoutés",
                 employes: response.data.result
             })
@@ -141,52 +124,45 @@ app.get('/annuaire/add=:id', (req, res) => {
     .catch((err) => renderError(res, err.message))
 })
 
-app.get('/annuaire/search/nom', (req, res) => {
-    res.render('recherche.twig', {
-        object:"nom",
-        title:"nom",
-        text:"Nom"
-    })
-})
-
-app.get('/annuaire/search/prenom', (req, res) => {
-    res.render('recherche.twig', {
-        object:"prenom",
-        title:"prénom",
-        text:"Prénom"
-    })
-})
-
-app.get('/annuaire/search/phone', (req, res) => {
-    res.render('recherche.twig', {
-        object:"phone",
-        title:"numéro de téléphone",
-        text:"0xxxxxxxxx"
-    })
-})
-
-app.get('/annuaire/search/date', (req, res) => {
-    res.render('rechercheDate.twig', {
-        object:"date",
-        title:"date d'entrée"
-    })
-})
-
-app.get('/annuaire/search/service', (req, res) => {
-    res.render('recherche.twig', {
-        object:"service",
-        title:"service",
-        text:"Service-du-contact"
-    })
-})
-
 //Lancement de l'application
 app.listen(port, () => console.log('Démarré sur le port ' + port))
 
 //Fonctions
-function renderError(res, errMsg) {
-    res.render('error.twig', {
-        object:'Recherche',
-        errorMsg: errMsg
+function renderCheck(res, searchObj, urlObj, response, params) {
+    if (response.data.status == 'success' && response.data.result != "")
+        renderResult(res, searchObj, response.data.result, params.toString(), urlObj)
+    else
+        renderError(res, searchObj, params.toString(), urlObj)
+}
+
+function renderResult(res, _searchObj, _employees, _param, _urlObj) {
+    res.render('result.twig', {
+        title: 'Recherche par ' + _searchObj,
+        param: 'Recherche : ' + _param,
+        employes:_employees,
+        searchObj: _urlObj
     })
+}
+
+function renderError(res, _searchObj, _param, _urlObj) {
+    res.render('error.twig', {
+        title: 'Recherche par ' + _searchObj,
+        id: 'Recherche : ' + _param,
+        param: 'Recherche : ' + _param,
+        searchObj: _urlObj
+    })
+}
+
+function renderSearch(res, _searchObj, _urlObj, _text) {
+    if (_text != undefined)
+        res.render('recherche.twig', {
+            object:_urlObj,
+            title:_searchObj,
+            text: _text
+        })
+    else 
+        res.render('rechercheDate.twig', {
+            object:_urlObj,
+            title:_searchObj,
+        })
 }
--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require('express'),
   mysql = require('promise-mysql'),
   config = require('./assets/config'),
   morgan = require('morgan')('dev'),
- {success, error} = require('./assets/functions'),
+ {success, checkResult} = require('./assets/functions'),
   axios = require('axios')
-var fs = require('fs')
 
-  
 mysql.createConnection({
   host: config.db.host,
   database: config.db.database,
@@ -17,7 +15,6 @@ mysql.createConnection({
 
     //Variables globales
     const app = express()
-    let MembersRouter = express.Router()
     let ManagerPerson = require('./assets/classes/managerperson-class')(db, config)
     let {Personne} = require('./assets/classes/person-class')
 
@@ -25,89 +22,56 @@ mysql.createConnection({
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }))
 
-    MembersRouter.route('/')
-
     //Envoie tous les membres
-    .get(async (req, res) => {
+    app.get('/annuaire', async (req, res) => {
       let getPerson = await ManagerPerson.getAll()
-      /*let Personnes = []
-      for (let i = 0; i < getPerson.length; i++) {
-        Personnes.push(new Personne(getPerson[i].nom, getPerson[i].prenom, getPerson[i].telephone, getPerson[i].date, getPerson[i].service))
-      }*/
       res.json(success(getPerson))
     })
-    //MembersRouter.route('/search')
 
     //Recherche par nom
     app.get('/annuaire/search/nom=:id', async (req, res) => {
-      if (req.params.id != "") {
-        let searchLName = await ManagerPerson.searchByLName(req.params.id)
-        res.json(success(searchLName))
-      }
-      else
-        res.json(error('Nom invalide'))
+      let searchLName = await ManagerPerson.searchByLName(req.params.id)
+      res.json(checkResult(searchLName))
     })
 
     //Recherche par prénom
     app.get('/annuaire/search/prenom=:id', async (req, res) => {
-      if (req.params.id != "") {
-        let searchFName = await ManagerPerson.searchByFName(req.params.id)
-        res.json(success(searchFName))
-      }
-      else
-        res.json(error('Nom invalide'))
+      let searchFName = await ManagerPerson.searchByFName(req.params.id)
+      res.json(checkResult(searchFName))
     })
 
     //Recherche par numéro de tel
     app.get('/annuaire/search/phone=:id', async (req, res) => {
-      if (req.params.id != "") {
-        let searchPhone = await ManagerPerson.searchByPhone(req.params.id)
-        res.json(success(searchPhone))
-      }
-      else
-        res.json(error('Nom invalide'))
+      let searchPhone = await ManagerPerson.searchByPhone(req.params.id)
+      res.json(checkResult(searchPhone))
     })
 
     //Recherche par service
     app.get('/annuaire/search/service=:id', async (req, res) => {
-      if (req.params.id != "") {
-        let searchService = await ManagerPerson.serchByService(req.params.id)
-        res.json(success(searchService))
-      }
-      else
-        res.json(error('Service invalide'))
+      let searchService = await ManagerPerson.serchByService(req.params.id)
+      res.json(checkResult(searchService))
     })
 
     //Recherche par date
     app.get('/annuaire/search/date=:id', async (req, res) => {
-      if (req.params.id != "") {
         let searchDate = await ManagerPerson.serchByDate(req.params.id)
-        res.json(success(searchDate))
-      }
-      else
-        res.json(error('Date invalide'))
+        res.json(checkResult(searchDate))
     })
 
     //Ajout d'un membre
     app.get('/annuaire/add=:id', async (req, res) => {
-      if (req.params.id != "") {
-        let RandomEmployee
-        let newEmployees = [] 
-        for (let i = 0; i < req.params.id; i++) {
-          await axios.get('https://randomapi.com/api/t724srr6?key=HQVB-2SUT-QOUU-1H1R')
-          .then(async (response) => {
-            RandomEmployee = response.data.results[0]
-            let addNew = ManagerPerson.addPerson(RandomEmployee.lname, RandomEmployee.fname, RandomEmployee.phone, RandomEmployee.date, RandomEmployee.serviceNum)
-            newEmployees.push(new Personne(RandomEmployee.lname, RandomEmployee.fname, RandomEmployee.phone, RandomEmployee.date, RandomEmployee.service))
-          })
-        }
-        res.json(success(newEmployees))
+      let newEmployees = [] 
+      for (let i = 0; i < req.params.id; i++) {
+        await axios.get('https://randomapi.com/api/t724srr6?key=HQVB-2SUT-QOUU-1H1R')
+        .then(async (response) => {
+          let RandomEmployee = response.data.results[0]
+          ManagerPerson.addPerson(RandomEmployee.lname, RandomEmployee.fname, RandomEmployee.phone, RandomEmployee.date, RandomEmployee.serviceNum)
+          newEmployees.push(new Personne(RandomEmployee.lname, RandomEmployee.fname, RandomEmployee.phone, RandomEmployee.date, RandomEmployee.service))
+        })
       }
-      else
-        res.json(error('Nombre invalide'))
+      res.json(success(newEmployees))
     })
 
-    app.use(config.rootAPI, MembersRouter)
     app.listen(config.port, () => console.log('Démarré sur le port '+config.port))
 
 }).catch((err) => {
